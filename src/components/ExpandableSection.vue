80b5839060f2d7d3af3fb8c685fafb1f035c2ee5
m3gan-quinn <149011015+m3gan-quinn@users.noreply.github.com>
2024-10-25 11:10:46 +0100

    EduPage

---
+++ src/components/ExpandableSection.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+      <h2 @click="toggleSection" class="cursor-pointer text-lg font-bold mb-2 text-center">
+        {{ title }}
+        <img :src="isOpen ? '/imgs/eduPageImgs/minus-sign.png' : '/imgs/eduPageImgs/add.png' " alt="Toggle Icon" class="w-6 h-6 ml-2 inline-block"/>
+      </h2>
+      <div v-show="isOpen" class="transition-all duration-300 ease-in-out">
+        <slot></slot>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      title: {
+        type: String,
+        required: true,
+      }, 
+       defaultOpen: {
+        type: Boolean,
+        default: false,
+    },
+    },
+  
+    data() {
+      return {
+        isOpen: this.defaultOpen,
+      };
+    },
+    methods: {
+      toggleSection() {
+        this.isOpen = !this.isOpen;
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .cursor-pointer {
+    cursor: pointer;
+  }
+  </style>
---
+++ src/components/InfoSection.vue
@@ -0,0 +1,22 @@
+
+<template>
+    <section class="py-12 bg-white">
+        <div class="container mx-auto">
+            <h2 class="text 2xl font-bold text-center mb-6">{{ title }}</h2>
+            <div class="flex flex-col md:flex-row justify-center items-center space-y-6 md::space-y-0 md:space-x-6">
+                <img :src="image" alt="Recycling Image" class="w-full md:w-1/2 rounded-lg shadow-md">
+                <p class="text-gray-600 md:w-1/2">{{ content }}</p>
+            </div>
+        </div>
+    </section>
+</template>
+<script>
+export default {
+  name: 'InfoSection',
+  props: {
+    title: String,
+    content: String,
+    image: String
+  }
+};
+</script>
