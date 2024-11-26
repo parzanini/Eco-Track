30fe912003ed0b5cd62d5387be88a8ace15f4db3
k00288477 <k00288477@student.tus.ie>
2024-11-26 18:51:58 +0000

    Create functionality for Logging a waste event
    - created API call to backend
    - updated waste log form

--- src/views/UserWasteLogForm.vue
+++ src/views/UserWasteLogForm.vue
@@ -4,38 +4,63 @@
 <script setup>
   import { ref } from 'vue';
   import UserValidation from "../components/userPortalComponents/userValidation.js";
+  import axios from "axios";
+  import router from "@/router/index.js";
   const userValidation = new UserValidation();
-
+  const user_id = JSON.parse(sessionStorage.getItem('user_id'));
   // State
   const itemDescription = ref('')
   const selectedCategory = ref('')
+  const selectedType = ref('');
+  const wasteType = ref([ 'Single Entry','12L bag','30L bag','50L bag'])
   const categories = ref([
   'Paper',
   'Plastic',
   'Organic',
   'General Waste',
-  'Other'
+  'General Recycling'
   ])
   const errorMessage = ref('');
-
+  const successMessage = ref('')
 
   // Methods
-  const submitForm = () => {
+  const submitForm = async () => {
     // validate inputs
     const validation = userValidation.userWasteValidation(itemDescription.value, selectedCategory.value);
 
-    if(!validation.isValid){
+    if (!validation.isValid) {
       errorMessage.value = validation.message;
       return;
     }
 
+    const formData = {
+      description: itemDescription.value,
+      category: selectedCategory.value,
+      wasteType: selectedType.value,
+      user_id: user_id
+    }
     // clear error message
     errorMessage.value = '';
 
+    // API call
+    try {
+      const response = await axios.post(`http://localhost/CI4-EcoTrack/public/insertWasteEvent`, formData,
+          { headers: { 'Content-Type': 'application/json', } });
+      // Display message on success code
+      if(response.status === 200 || response.status === 201){
+        // Display feedback to the user
+        successMessage.value = 'Waste Event Logged Successfully'
 
-    // send form data to backend
+        // Redirect after 5 seconds
+        setTimeout(() => {
+          router.push('/')
+        }, 3000)
+      }
 
-}
+    } catch (e) {
+      console.log(e)
+    }
+  }
 </script>
 
 <template>
@@ -81,7 +106,34 @@
           </label>
         </div>
       </div>
+
+      <div>
+        <label class="block text-sm mb-2">Waste Type:</label>
+        <div class="flex flex-wrap gap-2">
+          <label
+              v-for="type in wasteType"
+              :key="type"
+              :class="[
+              'px-4 py-2 rounded-[0.5rem] border-600 border-[#36BA24] cursor-pointer border',
+              selectedType === type
+                ? 'bg-green-500 text-white border-green-500'
+                : 'bg-white border-green-200 hover:bg-green-50'
+            ]"
+          >
+            <input
+                type="radio"
+                name="category"
+                :value="type"
+                v-model="selectedType"
+                class="hidden"
+            />
+            {{ type }}
+          </label>
+        </div>
+      </div>
 <div class="flex flex-col">
+  <span class="text-center font-semibold text-[#15803d] mt-4"
+    v-if="successMessage" >{{ successMessage }}</span>
   <button type="submit" class="px-16 py-1 my-5 text-white bg-blue-700 rounded-3xl w-[90%] mx-auto">Log Waste</button>
   <p class="">Note: Recycling more often will help you reach the next milestone</p>
 </div>
